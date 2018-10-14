19132d5ad5c2f83d32fd61aa81f373100c42d027
akshay2000saxena <41586370+akshay2000saxena@users.noreply.github.com>
2018-10-14 09:50:30 -0400

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            SceneManager.LoadScene(6);
+            
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
+	
+	
